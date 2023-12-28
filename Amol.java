011e604eb50404f86c250ab71d67f62a089519dd
EngineeringSham <creatives.garje@gmail.com>
2023-12-28 16:25:54 +0530

    First-comit

---
+++ Amol.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Amol{
+
+    public void getInput(){
+        System.out.println("Your Name is Amol Kailas Garje .....");
+    }
+
+    public static void main(String[] args) {
+          System.out.println("I am in Amol Class Method...");
+          Scanner mm=new Scanner(System.in);
+          String Name=mm.nextLine();
+
+          System.out.println("Your Nam is"+Name);
+
+    }
+    
+}   
